947d037019835bc32bbcc05fce41e3d1b55e023d
Dacey Nolan <daceyshirley@gmail.com>
2020-06-16 11:32:12 -0400

    feat: create button component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 
+import Button from "components/Button";
 import SearchBar from "components/SearchBar";
 
 function App() {
   return (
     <div className="App">
       <SearchBar />
+      <Button />
     </div>
   );
 }
---
+++ src/components/Button/Button.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button = () => {
+  return (
+    <button className="border-2 border-gray-400 px-3 py-2 font-medium rounded-lg text-gray-600 bg-white hover:bg-teal-400 hover:text-white focus:outline-none">
+      Search
+    </button>
+  );
+};
+
+export default Button;
