8a635bb1ec45e30b442779ea0b3f94a961a40b68
Dacey Nolan <daceyshirley@gmail.com>
2020-06-16 12:36:41 -0400

    feat: add get request logic

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import SearchBar from "components/SearchBar";
+import ghapi from "APIS/GitHub";
+
+const fetchUsers = ({ user }) => {
+  return ghapi.get(`/search/users?q=${user}`);
+};
 
 function App() {
+  //pass user information to App
+  const [users, setUsers] = useState([]);
+
+  //create loader
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (!user) return;
+    setIsLoading(true);
+    fetchUsers(user)
+      .then((response) => {
+        setUsers(response.data.data);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
+
   return (
     <div className="App">
       <SearchBar />
