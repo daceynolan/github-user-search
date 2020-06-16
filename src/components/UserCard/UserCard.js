7e3341853cf2add1a6f953a1c060181517e61652
Dacey Nolan <daceyshirley@gmail.com>
2020-06-16 16:20:53 -0400

    feat: add anchor tag

--- src/components/UserCard/UserCard.js
+++ src/components/UserCard/UserCard.js
@@ -4,12 +4,15 @@ import Avatar from "components/Avatar";
 
 const UserCard = ({ user }) => {
   return (
-    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+    <a
+      class="max-w-sm rounded overflow-hidden shadow-lg cursor-pointer"
+      href={user.html_url}
+    >
       <Avatar avatar={user.avatar_url} alt={user.login} />
       <div class="px-6 py-4">
         <div class="font-bold text-xl mb-2">{user.login}</div>
       </div>
-    </div>
+    </a>
   );
 };
 
