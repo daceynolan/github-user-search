210976a7d17c74a25f7025816f3cc71060b7021c
Dacey Nolan <daceyshirley@gmail.com>
2020-06-16 11:50:24 -0400

    chore: refactor code to more reusable components

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 
-import Button from "components/Button";
 import SearchBar from "components/SearchBar";
 
 function App() {
   return (
     <div className="App">
       <SearchBar />
-      <Button />
     </div>
   );
 }
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 
+import Button from "components/Button";
+import Input from "components/Input";
+
 const SearchBar = () => {
   const [searchTerm, setSearchTerm] = useState("");
 
@@ -14,13 +17,8 @@ const SearchBar = () => {
   return (
     <div className="flex justify-center">
       <form onSubmit={(e) => onFormSubmit(e)}>
-        <input
-          className="border-2 border-gray-400 hover:border-teal-400 focus:border-teal-400 focus:outline-none px-3 py-2 font-medium rounded-lg"
-          placeholder="Search"
-          type="text"
-          value={searchTerm}
-          onChange={onInputChange}
-        />
+        <Input onChange={(event) => onInputChange(event)} value={searchTerm} />
+        <Button />
       </form>
     </div>
   );
