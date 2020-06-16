f90fa1f975140b6b1ca134269a3b96672a22aaed
Dacey Nolan <daceyshirley@gmail.com>
2020-06-16 19:44:15 -0400

    feat: add pagination

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,20 +5,26 @@ import Paginate from "components/Paginate";
 import SearchBar from "components/SearchBar";
 import UserCard from "components/UserCard";
 
+const per_page = 9;
+
 const App = () => {
   const [users, setUsers] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
   const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
 
   const fetchUsers = useCallback(async () => {
     const response = await fetch(
-      `https://api.github.com/search/users?q=${searchTerm}&per_page=6&page=${page}`
+      `https://api.github.com/search/users?q=${searchTerm}&per_page=${per_page}&page=${page}`
     );
     const data = await response.json();
 
     setUsers(data.items);
+    // Calculated page count since GitHub API does not give total page count
+    setTotalPages(Math.ceil(data.total_count / per_page));
   }, [searchTerm, page]);
 
+  //try catch block for the errors
   useEffect(() => {
     fetchUsers();
   }, [page, searchTerm, fetchUsers]);
@@ -32,22 +38,27 @@ const App = () => {
     <>
       <SearchBar onFormSubmit={handleSearch} />
       <CenteringLayout>
+        {!users && <div>Please search for a user</div>}
         <div className="p-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10">
           {users?.map((user) => {
             return <UserCard key={user.id} user={user} />;
           })}
         </div>
       </CenteringLayout>
-      {/* <Paginate
-        // React-paginate is 0 indexed, so subtract 1
-        forcePage={page - 1}
-        pageCount={resolvedData?.meta.totalPages}
-        onPageChange={(selectedItem) => {
-          // increment page number since it is 0 indexed
-          const page = selectedItem.selected + 1;
-          setSearchParams({ page });
-        }}
-      /> */}
+      <div className="flex justify-center mb-4">
+        {totalPages > 1 && (
+          <Paginate
+            // React-paginate is 0 indexed, so subtract 1
+            forcePage={page - 1}
+            pageCount={totalPages}
+            onPageChange={(selectedItem) => {
+              // increment page number since it is 0 indexed
+              const page = selectedItem.selected + 1;
+              setPage(page);
+            }}
+          />
+        )}
+      </div>
     </>
   );
 };
--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -3,7 +3,7 @@ import React from "react";
 const Input = ({ ...props }) => {
   return (
     <input
-      className="border-2 border-gray-400 px-3 py-2 font-medium rounded-lg text-gray-600 hover:border-teal-400 focus:border-teal-400 focus:outline-none"
+      className="border-2 border-gray-400 px-3 py-2 font-medium rounded-lg text-gray-600 hover:border-teal-500 focus:border-teal-500 focus:outline-none"
       placeholder="Search"
       type="text"
       {...props}
